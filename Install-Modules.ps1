3dac31b70870dc468af77ab3d83f25cfb1da1ab0
Nilesh Gule <vn_nilesh@yahoo.com>
2020-07-04 19:32:00 +0800

    Added profiles and install module scripts

---
+++ Install-Modules.ps1
@@ -0,0 +1,6 @@
+#Install posh-git and oh-my-posh PowerShell modules
+Install-Module posh-git -AllowPrerelease -Force
+Install-Module oh-my-posh -AllowPrerelease -Force
+
+#Install PSReadLine
+Install-Module PSReadLine -AllowPrerelease -Force
